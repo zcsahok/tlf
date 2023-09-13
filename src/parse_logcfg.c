2ce436935dfcee067c824fd03d82aa80c272f40a
Zoltan Csahok <ha5cqz@gmx.com>
2023-09-13 22:40:55 +0200

    Apply and enforce astyle (#408)

--- src/parse_logcfg.c
+++ src/parse_logcfg.c
@@ -713,12 +713,12 @@ static int cfg_countrylist(const cfg_arg_t arg) {
 	fclose(fp);
 
     } else {	/* not a file */
-        char *colon = strchr(buffer, ':');
-        if (colon != NULL) {
-            country_list_raw = colon + 1;   // skip optional contest name
-        } else {
-            country_list_raw = buffer;
-        }
+	char *colon = strchr(buffer, ':');
+	if (colon != NULL) {
+	    country_list_raw = colon + 1;   // skip optional contest name
+	} else {
+	    country_list_raw = buffer;
+	}
     }
 
     if (country_list_raw == NULL) {
@@ -755,8 +755,8 @@ static int cfg_countrylist(const cfg_arg_t arg) {
     }
 
     setcontest(whichcontest);
-    if(buffer != NULL)
-        free(buffer);
+    if (buffer != NULL)
+	free(buffer);
 
     return PARSE_OK;
 }
@@ -848,7 +848,7 @@ static int cfg_continentlist(const cfg_arg_t arg) {
 
     setcontest(whichcontest);
     if (buffer != NULL)
-        free(buffer);
+	free(buffer);
     return PARSE_OK;
 }
 

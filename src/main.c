41a7e72ad4cc34e22c0d2c027382f38675a4e283
Thomas Beierlein <dl1jbe@darc.de>
2024-01-07 17:30:10 +0100

    Fix startup if HAMLIB_KEYER selected and rig is off or disabled at start (#416)
    * Fix startup if HAMLIB_KEYER selected and rig is off or disabled at start
    
    Otherwise TLF will abort unconditionally.

--- src/main.c
+++ src/main.c
@@ -414,7 +414,7 @@ bool itumult = false;		/* to add the ability of ITU zones to be multiplier */
 char itustr[3];
 
 bool nopacket = false;		/* set if tlf is called with '-n' */
-bool no_trx_control = false;	/* set if tlf is called with '-r' */
+bool trx_control_disabled = false;	/* set if tlf is called with '-r' */
 bool convert_cabrillo = false;  /* set if the arg input is a cabrillo */
 
 int bandweight_points[NBANDS] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0};
@@ -466,7 +466,7 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 	    nopacket = true;
 	    break;
 	case 'r':
-	    no_trx_control = true; // disable radio control
+	    trx_control_disabled = true; // disable radio control
 	    break;
 	case 'i':
 	    convert_cabrillo = true;
@@ -756,7 +756,7 @@ static int databases_load() {
 
 static void hamlib_init() {
 
-    if (no_trx_control) {
+    if (trx_control_disabled) {
 	trx_control = false;
     }
 
@@ -778,6 +778,7 @@ static void hamlib_init() {
 	    exit(1);
 	}
 	trx_control = false;
+	trx_control_disabled = true;
 	showmsg("Disabling rig control!");
 	sleep(1);
     }
@@ -870,6 +871,11 @@ static void keyer_init() {
 
     if (cwkeyer == HAMLIB_KEYER) {
 	showmsg("CW-Keyer is Hamlib");
+	if (trx_control_disabled) {
+	    showmsg("Radio control disabled - no keying!");
+	    sleep(2);
+	    return;
+	}
 	if (!trx_control) {
 	    showmsg("Radio control is not activated!!");
 	    sleep(1);

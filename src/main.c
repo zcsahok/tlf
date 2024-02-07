a226654da27b56af71864cb538541d9a3c9c296d
Zoltan Csahok <ha5cqz@gmx.com>
2024-02-07 22:37:12 +0100

    Terminate background thread gracefully (#424)
    * Terminate background thread gracefully
    
    * fix background thread pause/resume action naming

--- src/main.c
+++ src/main.c
@@ -960,7 +960,7 @@ static void mark_GPL_seen() {
  */
 static void tlf_cleanup() {
     if (pthread_self() != background_thread) {
-	pthread_cancel(background_thread);
+	terminate_background_process();
 	pthread_join(background_thread, NULL);
     }
 

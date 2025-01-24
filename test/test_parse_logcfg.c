8bbe4a6f501a28838bf87cc8b1ea26e3b2263b5d
Zoltan Csahok <ha5cqz@gmx.com>
2025-01-24 22:42:07 +0100

    Fix rig mode sync and allow disabling it (#458)
    Configurable via RIG_MODE_SYNC, default is on
    
    ---------
    
    Co-authored-by: Marcin SP6MI <darvark@gmail.com>
    Co-authored-by: Thomas Beierlein <tomjbe@gentoo.org>

--- test/test_parse_logcfg.c
+++ test/test_parse_logcfg.c
@@ -185,6 +185,7 @@ int setup_default(void **state) {
     change_rst = false;
     fixedmult = 0.0;
     exclude_multilist_type = EXCLUDE_NONE;
+    rig_mode_sync = true;
     rigptt = 0;
     minitest = 0;
     cqmode = CQ;
@@ -429,6 +430,7 @@ static bool_true_t bool_trues[] = {
     {"MIXED", &mixedmode},
     {"IGNOREDUPE", &ignoredupe},
     {"USE_CONTINENTLIST_ONLY", &continentlist_only},
+    {"RIG_MODE_SYNC", &rig_mode_sync},
     {"RADIO_CONTROL", &trx_control},
     {"PORTABLE_MULT_2", &portable_x2},
     {"WYSIWYG_MULTIBAND", &wysiwyg_multi},

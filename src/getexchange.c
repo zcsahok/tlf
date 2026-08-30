80323dae003a05ca0352172be1804e1aeaa5d73d
zcsahok <ha5cqz@gmx.com>
2026-08-30 14:13:12 +0000

    remove commented-out branch

--- src/getexchange.c
+++ src/getexchange.c
@@ -246,21 +246,6 @@ int getexchange(void) {
 		break;
 	    }
 
-	    /* I cannot find any reference for this key combination in my
-	     * CT ver 9 documentation.  As it is, most X window managers
-	     * will trap this combination for the window menu so would
-	     * only be useful on the console.
-	     *
-	     * - N0NB
-	     */
-	    /* case 160: {	// For CT compatibility Meta-<Space> (M- ) */
-	    /*     if (ctcomp != 0) { */
-	    /*         send_standard_message(1);		// F2 */
-
-	    /*     } */
-	    /*     break; */
-	    /* } */
-
 	    /* '+', send TU and log in CT mode */
 	    case '+': {
 		if (ctcomp && (strlen(current_qso.call) > 2)) {
